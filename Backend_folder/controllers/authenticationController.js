5a7d10e0fe013913cead9f7f9d18bd989ad25719
lokesh <lokeshtamatapu@gmail.com>
2025-04-07 13:16:24 +0530

    updated credintials

--- Backend_folder/controllers/authenticationController.js
+++ Backend_folder/controllers/authenticationController.js
@@ -18,10 +18,8 @@ exports.createUser = async (req, res) => {
             return res.status(400).json({ message: "User already exists" });
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser = new User({ name, email, password: hashedPassword }); 
-
+        const newUser = new User({ name, email, password }); 
 
         await newUser.save();
         res.status(201).json({ message: "User registered successfully" });
